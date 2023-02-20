b8d81285cd2998a7ae7555084a094908ceaff59e
xikam01 <cabdiraxmaanxikam190@gmil.com>
2023-02-20 00:31:57 -0800

    Donw

--- Store managment system/Program.cs
+++ Store managment system/Program.cs
@@ -16,7 +16,7 @@ namespace Store_managment_system
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard1());
+            Application.Run(new splash());
         }
     }
 }
